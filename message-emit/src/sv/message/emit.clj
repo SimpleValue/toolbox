2f0255c68d01af4ffc98a4d30f2fa929d57c2890
Max Weber <weber.maximilian@gmail.com>
2021-02-02 08:21:38 +0100

    Removed multimethod, passing a map of message handlers as parameter now.

--- message-emit/src/sv/message/emit.clj
+++ message-emit/src/sv/message/emit.clj
@@ -1,20 +1,12 @@
 (ns sv.message.emit
-  (:require [sv.message.handle :as handle]
-            [sv.util.core :as util-core]
-            [sv.message.logging :as message-logging]))
-
-(defn log-message
-  "Logs the `message`.
-
-   Use `alter-var-root` on this var to globally set a different
-   message logging implemenation."
-  [message]
-  (message-logging/log-message message))
+  (:require [sv.util.core :as util-core]))
 
 (defn- dispatch!
-  [message]
+  [{:keys [message/handlers message/log]} message]
   (try
-    (handle/handle! message)
+    (when-let [handler (get handlers
+                            (:message/type message))]
+      (handler message))
     (catch Throwable e
       ;; the error message should only be emitted once. The flag
       ;; `::message-dispatch-error` signals that the exception was
@@ -22,9 +14,9 @@
       ;; does not emit another error message that only wraps the
       ;; first error message:
       (when-not (::message-dispatch-error (ex-data e))
-        (log-message {:message/type :error/fault
-                      :message message
-                      :exception (pr-str e)}))
+        (log {:message/type :error/fault
+              :message message
+              :exception (pr-str e)}))
       (throw (ex-info "message dispatch failed"
                       {::message-dispatch-error true}
                       e)))))
@@ -47,13 +39,14 @@
 
 (defn emit!
   "Logs and dispatches the `message`."
-  [message]
+  [{:keys [message/log] :as registry} message]
   (let [msg (cond-> (-> message
                         (add-message-uuid)
                         (add-timestamp))
               (not (:message/emit message))
               (assoc :message/emit emit!))]
-    (log-message msg)
-    (dispatch! msg)
+    (log msg)
+    (dispatch! registry
+               msg)
     true)
   )
--- message-emit/src/sv/message/handle.clj
+++
@@ -1,7 +0,0 @@
-(ns sv.message.handle)
-
-;; Concept:
-;;
-;; Use `defmethod` to register a handler for a `:message/type`.
-
-(defmulti handle! :message/type)
